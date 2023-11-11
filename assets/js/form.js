13cf9c47a2a73d7129944eaa7a7dbb4ebc647778
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-11-11 00:46:18 -0300

    validacao campos inputs

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,12 @@
-const params = new URLSearchParams(window.location.search).values();
-console.log(params)
+const params = new URLSearchParams(window.location.search).entries();
+const arrayInputValues = [] 
+const msgErrorEmail = document.querySelector('#email + .msgError');
+const msgErrorPassword = document.querySelector('#password + .msgError');
+const msgErrorConfPassword = document.querySelector('#confPassword + .msgError');
 
-for(const value of params) {
-  console.log(value)
+for(const [key, value] of params) {
+  console.log(key, value)
+  // arrayInputValues.push(value)
+  // validInputs(key, value)
 }
-// for(const key of params.values()) {
-//   console.log(key)
-// }
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,44 @@
 const formSubmit = document.querySelector('#formSignUp');
 
-formSubmit.addEventListener('submit', handlClick)
-
-function handlClick(event) {
-  // event.preventDefault()
-  const url = new URLSearchParams(window.location.search)
-  console.log(url)
+const validFieldsInput = {
+  email: {
+    regex: '^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$',
+    error: 'Email Inválido!',
+  },
+  password: {
+    regex: '(?=.*[A-Z])(?=.*[!@#$&*_-])(?=.*[0-9].*[0-9])(?=.*[a-z]).{8}$',
+    error: 'A senha deve conter letra maiuscula, letra minuscula, pelo menos dois números e 1 caracter especial.',
+    value: '',
+  },
+  confPassword: {
+    error: 'As senhas devem ser iguais',
+  }
 }
 
+// Inputs Validation:
+function validInputs(key, value) {
+  const msgError = document.querySelector(`#${key} + .msgError`);
+  msgError.textContent = '';
+
+  const mountRegex = new RegExp(validFieldsInput[key]?.regex, 'i');
+  const validRegex = mountRegex.test(value.trim());
+  if(key == 'password') validFieldsInput[key].value = value;
+
+  if(key == 'confPassword' && validFieldsInput[key].value != value) return msgError.textContent = validFieldsInput[key].error;
+
+  if(!validRegex) return msgError.textContent = validFieldsInput[key].error;
+  else return true
+}
+
+onsubmit = (event) => {
+  event.preventDefault();
+  const allInputs = document.querySelectorAll('input')
+
+  for(const input of allInputs) {
+    if(validInputs(input.name, input.value) !== true) return;
+  }
+
+  if(allInputs[2].value != allInputs[3].value) return;
+
+  event.currentTarget.submit();
+}
