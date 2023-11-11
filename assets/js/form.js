22c56a4503a8ffa9ca0b7e5025f3332f440cfb2d
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-11-11 19:34:17 -0300

    update

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,12 +1,7 @@
 const params = new URLSearchParams(window.location.search).entries();
 const arrayInputValues = [] 
-const msgErrorEmail = document.querySelector('#email + .msgError');
-const msgErrorPassword = document.querySelector('#password + .msgError');
-const msgErrorConfPassword = document.querySelector('#confPassword + .msgError');
 
 for(const [key, value] of params) {
   console.log(key, value)
-  // arrayInputValues.push(value)
-  // validInputs(key, value)
+  arrayInputValues.push(value)
 }
-
