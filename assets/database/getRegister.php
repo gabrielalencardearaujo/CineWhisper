2628de57e0059c6e1332cc764c6d613a7b0059d2
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-11-21 18:45:15 -0300

    registrando novo usuario

--- assets/database/getRegister.php
+++ assets/database/getRegister.php
@@ -1,16 +1,31 @@
 <?php 
   include('./connectDB.php');
-
   session_start();
 
-  // $_SESSION['']
-
   $form = $_POST;
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  
+  $selectSQL = "SELECT * FROM tb_users WHERE $email = tb_users.email"; 
+  $resSelect = $conn->query($selectSQL);
 
-  echo '<pre>';
-  echo $form['name'] . '<br>';
-  echo $form['email'] . '<br>';
-  echo $form['password'];
-  echo '</pre>';
-  echo '<hr>';
+  $qtd = $resSelect->num_rows;
+
+  if($qtd > 0) {
+    header('Location: ../../index.php?register=UserExist');
+  }
+
+  $_SESSION['User'] = $name;
+
+  $insertSQL = "INSERT INTO tb_users (name, email, password) VALUES ('{$name}', '{$email}', '{$password}')";
+
+  $resInsert = $conn->query($insertSQL);
+
+  if($resInsert == true) {
+    header('Location: ../../sendForm.php');
+  } else {
+    header('Location: ../../index.php?register=ErrorRegister');
+  }
+
 ?>
